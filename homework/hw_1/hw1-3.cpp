cf1f6f7e03e9e1bc07da477baaf0ac0e96ce991e
EdyTheCow <git@edy.io>
2020-01-28 19:25:57 -0800

    hw1

--- homework/hw_1/hw1-3.cpp
+++ homework/hw_1/hw1-3.cpp
@@ -1,80 +1,115 @@
-/* Title: hw1-3.cpp​
- * Abstract: This program reads data from file and draw a horizontal histogram for the numbers
+/* Title: hw1-1.cpp​
+ * Abstract: This program reads data from file and displays them in formatted manner
  * Author: Edgaras Slezas
  * Email: eslezas@csumb.edu
- * Estimate: 4h
- * Date: 01/25/2020
+ * Estimate: 5h
+ * Date: 01/24/2020
 */
 
 #include <iostream>
 #include <fstream>
-#include <array>
+#include <climits>
 using namespace std;
 
 int main() {
 
-  cout << "Enter input file name: ";
-  string file_name;
-  cin >> file_name;
+    int max = INT_MAX;
 
-  // Open the input file.
-  // Variable "f_input" is an input file stream object
-  // from which you can read data similarly to
-  // the standard input stream cin.
-  ifstream f_input(file_name);
+    cout << "Enter input file name: ";
+    string file_name;
+    cin >> file_name;
 
-  if (f_input.fail()) {
-      cout << "Error: Input file open failed.\n";
-      return 0;
-  }
+    // Open the input file.
+	// Variable "f_input" is an input file stream object
+	// from which you can read data similarly to
+	// the standard input stream cin.
+    ifstream f_input(file_name);
 
-  int size;
-  int value[7];
-  int n;
-  int maxNumber;
-  int counter = 0;
-
-  cout << endl;
-  cout << "===== Vertical Histogram =====" << endl;
-
-  while (f_input >> n) {
-    value[counter] = n;
-    counter++;
-  }
-
-  maxNumber = value[0];
-
-  // Check if maxNumber is the highest number
-  for (int i = 0; i < 7; i++) {
-    if (value[i] > maxNumber) {
-      maxNumber = value[i];
+    if (f_input.fail()) {
+        cout << "Error: Input file open failed.\n";
+        return 0;
     }
-  }
 
-  // Print stars and spaces
-  for(int i = maxNumber; i >= 1; i--) {
+    int size;
 
-    for(int n = 0; n < 7; n++) {
-      if(value[n] >= i) {
-          cout << ("* ");
-      }
-      else {
-        cout << ("  ");
+	// Read the first number in the file.
+    f_input >> size;
+
+    int values[30];
+    int output[30];
+
+    // Read a value from the file and assign it to array value
+    for (int i=0; i < size; i++) {
+      f_input >> output[i];
+
+      // find min number
+      if (output[i] < max) {
+        max = output[i];
       }
     }
+
+    for (int i = 0; i < 30; i++){
+      values[i] = INT_MAX;
+    }
+
+    int newSize = 0;
+
+    for (int i = 0; i < size; i++) {
+
+      for (int n = 0; n < size; n++) {
+
+        if (values[n] != INT_MAX) {
+          if (output[i] == values[n]) {
+
+            break;
+          }
+
+        } else {
+          values[n] = output[i];
+          newSize++;
+          break;
+        }
+
+        }
+
+    }
+
+
+    int counter[30] = {0};
+
+    for (int i = 0; i < newSize; i++) {
+      for (int n = 0; n < size; n++) {
+
+        if (values[i] == output[n]) {
+          counter[i]++;
+        }
+
+
+        }
+
+    }
+
+
+
+
+    cout << "Min number: " << max << endl;
     cout << endl;
-  }
 
-  cout << ("-------------") << endl;
+    cout << "Number   Count" << endl;
 
-  // Print numbers from file
-  for (int i = 0; i < 7; i++) {
-    cout << value[i] << (" ");
-  }
-  cout << endl;
+    // print all numbers
+    for (int i = 0; i < newSize; i++) {
 
-  // Close the input file stream object.
-  f_input.close();
+      cout << values[i] << "        " <<  counter[i] << endl;
+    }
 
-  return 0;
+
+
+
+
+    // Close the input file stream object.
+    f_input.close();
+
+
+    return 0;
 }
